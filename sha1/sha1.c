676436cb1d7c749b3b8f029b037c3f3400215534
Michael Myer <misc.myer@zoho.com>
2022-12-25 17:13:23 +0000

    erase stack

--- sha1/sha1.c
+++ sha1/sha1.c
@@ -13,10 +13,10 @@ A million repetitions of "a"
 */
 /*
  changes 2022 misc. Got this down from 7.5kB to 1kB.
- bsd license 3clause. It's just not my fault.
+ bsd 3clause license. It's just not my fault.
 
  If SHA1HANDSOFF is not defined, the content of the buffer to hash is changed (!)
- In favour of codesize this now works only on little endian (amd64, intel x64) machines.
+ In favor of codesize this now works only on little endian (amd64, intel x64) machines.
  if you'd like to have the stack variables overwritten, 
  undefine the bzero macro below.
  Albite I'd suggest, instead of trying (! this is not guaranteed to work) to erase
@@ -31,12 +31,15 @@ A million repetitions of "a"
  it's squeezed enough, to work only with registers.
  No memory access needed, besides reading the buffer to hash.
 
- 
-
  */
 
 //#define LITTLE_ENDIAN /* This should be #define'd already, if true. */
-/* #define SHA1HANDSOFF * Copies data before messing with it. */
+
+#define SHA1HANDSOFF  /* Copies data before messing with it. */
+#define SHA1_ERASE // overwrite data at the stack
+
+
+
 #ifndef MLIB
 #include <stdio.h>
 #include <string.h>
@@ -83,7 +86,11 @@ typedef struct{
 static void SHA1Transform( uint32_t state[5], unsigned char buffer[64] ){
 #ifdef SHA1HANDSOFF
     uint32_t block[16];
-    memcpy(block, buffer, 64);
+    //memcpy(block, buffer, 64);
+	 for ( int a = 0; a < 16; a++ ){ // copy and swap byteorder
+		 asm( "mov %1,%0\n bswap %0" : "=r"(*(block+a)) : "m"(*((uint*)buffer+a)) );
+	 }
+
 #else
     /* The following had better never be used because it causes the
      * pointer-to-const buffer to be cast into a pointer to non-const.
@@ -92,12 +99,13 @@ static void SHA1Transform( uint32_t state[5], unsigned char buffer[64] ){
      */ /* That's not been my idea. but spares 20 Bytes. I changed the declaration,
 			  removed the const, added the warning. misc */
     uint32_t *block = (uint32_t*)buffer;
+	for ( uint32_t *pi = block; pi<block+16; pi++ )
+		BSWAP(*pi);
 //#warning buffer will be changed (SHA1Update(buffer) -> SHA1Transform(buffer) ) 
 //	 define SHA1HANDSOFF if necessary
 #endif
 
-	for ( uint32_t *pi = block; pi<block+16; pi++ )
-		BSWAP(*pi);
+	// repl array with vars?
 #if 1
 #define A0 ar[0]
 #define A1 ar[1]
@@ -153,8 +161,8 @@ static void SHA1Transform( uint32_t state[5], unsigned char buffer[64] ){
 		
 		// rotate registers
 		uint32_t t = ar[0];
-		for ( int i2=0; i2<4; i2++ )
-			ar[i2] = ar[i2+1];
+		for ( int a = 0; a<4; a++ )
+			ar[a] = ar[a+1];
 		ar[4] = t;
 
 	}
@@ -167,84 +175,74 @@ static void SHA1Transform( uint32_t state[5], unsigned char buffer[64] ){
 /* SHA1Init - Initialize new context */
 
 static void SHA1Init( SHA1_CTX * context ){
-    /* SHA1 initialization constants */
-    context->state[0] = 0x67452301;
-    context->state[1] = 0xEFCDAB89;
-    context->state[2] = 0x98BADCFE;
-    context->state[3] = 0x10325476;
-    context->state[4] = 0xC3D2E1F0;
-    context->count[0] = context->count[1] = 0;
+	/* SHA1 initialization constants */
+	context->state[0] = 0x67452301;
+	context->state[1] = 0xEFCDAB89;
+	context->state[2] = 0x98BADCFE;
+	context->state[3] = 0x10325476;
+	context->state[4] = 0xC3D2E1F0;
+	context->count[0] = context->count[1] = 0;
 }
 
 
 /* Run your data through this. */
 
 static void SHA1Update( SHA1_CTX * context, unsigned char *data, uint32_t len){
-    uint32_t i,j;
+	uint32_t i,j;
 
-    j = context->count[0];
-    if ((context->count[0] += len << 3) < j)
-        context->count[1]++;
-    context->count[1] += (len >> 29);
-    j = (j >> 3) & 63;
-    if ((j + len) & ~63){ //> 63)
-        memcpy(&context->buffer[j], data, (i = 64 - j));
-        SHA1Transform(context->state, context->buffer);
-        for (; i + 63 < len; i += 64){
-            SHA1Transform(context->state, &data[i]);
-        }
-        j = 0;
-    }
-    else
-        i = 0;
-    memcpy(&context->buffer[j], &data[i], len - i);
+	j = context->count[0];
+	if ((context->count[0] += len << 3) < j)
+		context->count[1]++;
+	context->count[1] += (len >> 29);
+	j = (j >> 3) & 63;
+	if ((j + len) & ~63){ //> 63)
+		memcpy(&context->buffer[j], data, (i = 64 - j));
+		SHA1Transform(context->state, context->buffer);
+		for (; i + 63 < len; i += 64)
+			SHA1Transform(context->state, &data[i]);
+		j = 0;
+	} else {
+		i = 0;
+	}
+	memcpy(&context->buffer[j], &data[i], len - i);
 }
 
+// overwrite 1kB at the stack with 0
+void __attribute__((noipa,noinline))erase_stack(){
+	uint64_t stack[128];
+	asm volatile("rep stosq" : : "D"(stack), "c"(128), "a"(0): "cc" );
+}
 
 /* Add padding and return the message digest. */
 
 static void SHA1Final( unsigned char digest[20], SHA1_CTX * context ){
-    unsigned i;
-    unsigned char finalcount[8];
-    unsigned char c;
+	unsigned i;
+	unsigned char finalcount[8];
+	unsigned char c;
 
-#if 0    /* untested "improvement" by DHR */
-    /* Convert context->count to a sequence of bytes
-     * in finalcount.  Second element first, but
-     * big-endian order within element.
-     * But we do it all backwards.
-     */
-    unsigned char *fcp = &finalcount[8];
+	for (i = 0; i < 8; i++)
+		finalcount[i] = (unsigned char) ((context->count[!(i&~3)] >> 
+					((3 - (i & 3)) << 3)));    
 
-    for (i = 0; i < 2; i++)
-    {
-        uint32_t t = context->count[i];
+	c = 128;
+	SHA1Update(context, &c, 1);
+	while ((context->count[0] & 504) != 448){
+		c = 0;
+		SHA1Update(context, &c, 1);
+	}
+	SHA1Update(context, finalcount, 8); /* Should cause a SHA1Transform() */
+	for (i = 0; i < 20; i++)
+		digest[i] = (unsigned char) ((context->state[i >> 2] >> ((3 - (i & 3)) << 3)) );
 
-        int j;
-
-        for (j = 0; j < 4; t >>= 8, j++)
-            *--fcp = (unsigned char) t}
-#else
-    for (i = 0; i < 8; i++){
-        finalcount[i] = (unsigned char) ((context->count[!(i&~3)] >> 
-					  ((3 - (i & 3)) << 3)));    
-        //finalcount[i] = (unsigned char) ((context->count[(i >= 4 ? 0 : 1)] >> ((3 - (i & 3)) << 3)) & 255);      /* Endian independent */
-    }
+#ifdef SHA1_ERASE
+	/* Wipe variables */
+	//bzero(context, sizeof(*context));
+	asm volatile( "rep stosq" : : "D"(context), "ecx"(sizeof(context)>>3) );
+	//bzero(&finalcount, sizeof(finalcount));
+	asm volatile( "mark:\nmovq $0,%0\n" : "+m"(*(ulong*)finalcount) ); // shoot at the stack ..
+	erase_stack();
+	//asm volatile( "stosq" :: "D"(&finalcount) );
 #endif
-    c = 0200;
-    SHA1Update(context, &c, 1);
-    while ((context->count[0] & 504) != 448){
-        c = 0000;
-        SHA1Update(context, &c, 1);
-    }
-    SHA1Update(context, finalcount, 8); /* Should cause a SHA1Transform() */
-    for (i = 0; i < 20; i++){
-        digest[i] = (unsigned char)
-            ((context->state[i >> 2] >> ((3 - (i & 3)) << 3)) );
-    }
-    /* Wipe variables */
-    bzero(context, sizeof(*context));
-    bzero(&finalcount, sizeof(finalcount));
 }
 
 
